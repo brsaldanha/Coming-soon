7522e5ba1fddf8ec06ef13a7d075f0da9cc4f851
Bruno de Azevedo Brant Saldanha <brsaldanha@gmail.com>
2023-04-05 21:40:31 -0700

    Add files via upload
    Replacing the main.js file.

--- main.js
+++ main.js
@@ -7,32 +7,28 @@ const fromGroupEl = document.querySelector('.form-group');
 let errors = [];
 
 btn.addEventListener('click', e => {
-  // remove success class
   textErrorEl.classList.remove('text-success');
-
-  // reset errors
   errors = [];
-  // prevent default action of form
   e.preventDefault();
-  // validate email address
+  
   const email = inputEl.value;
 
   if (email == '' || email === undefined) {
-    errors.push('Please provide us your email');
-    // show error
+    errors.push('Please type your email');
+    // Present an error
     iconErrorEl.style.display = 'block';
     textErrorEl.innerText = errors[0];
   } else if (!email.match(RegEmail)) {
-    errors.push('Please provide us your valid email');
-    // show error icon
+    errors.push('Please type a valid email');
+    // Present error icon
     iconErrorEl.style.display = 'block';
     textErrorEl.innerText = errors[0];
   }
 
   if (!errors.length > 0) {
-    // hide error icon
+    // Hide error icon
     iconErrorEl.style.display = 'none';
     textErrorEl.classList.add('text-success');
-    textErrorEl.innerText = 'Thank you for subscribing to our newsletter.';
+    textErrorEl.innerText = 'Thank you for subscribing to the newsletter.';
   }
 });
